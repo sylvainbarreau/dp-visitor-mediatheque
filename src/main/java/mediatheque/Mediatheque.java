73757c92d9d047184e2d7423728e5ea1811a883e
Rémi Bastide <Remi.Bastide@irit.fr>
2022-11-10 15:34:58 +0100

    Added Main class

--- src/main/java/mediatheque/Mediatheque.java
+++ src/main/java/mediatheque/Mediatheque.java
@@ -5,19 +5,8 @@ import java.util.List;
 
 
 public class Mediatheque {
-	private List<Item> items = new LinkedList<Item>();
+	private final List<Item> items = new LinkedList<>();
 
-	public static void main(String[] args) {
-		Mediatheque mediatheque = new Mediatheque();
-		
-		mediatheque.addItem( new Book("J.R.R. Tolkien", "Le seigneur des anneaux"));
-		mediatheque.addItem( new Book("Philip K. Dick", "Le Maître du haut chateau"));
-		mediatheque.addItem( new CD(12, "Sergeant Peppers"));
-		mediatheque.printCatalog();
-		//mediatheque.printOnlyBooks();
-		//mediatheque.printOnlyCDs();		
-	}
-	
 	public void addItem(Item i) {
 		items.add(i);
 	}
